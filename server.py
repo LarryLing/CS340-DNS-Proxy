fec9b2cfa9d81d41b0c7ae07971dd85c9aff8bf9
Larry Ling <larryling.main@gmail.com>
2025-10-05 17:55:34 -0500

    implemented basic message sending and recieving

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 import socket
 
-PORT = 53
-SERVER_IP = "8.8.8.8"
+PORT = 1053
+SERVER_IP = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER_IP, PORT)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(ADDR)
+proxy = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+proxy.bind(ADDR)
+
+proxyMessage, proxyAddress = proxy.recvfrom(2048)
+print(f"received {proxyMessage} from {proxyAddress}")
+
+upstream_resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+upstream_resolver.sendto(proxyMessage, ("8.8.8.8", 53))
+resolverMessage, resolverAddress = upstream_resolver.recvfrom(2048)
+print(f"received {resolverMessage} from {resolverAddress}")
+
+proxy.sendto(resolverMessage, proxyAddress)
